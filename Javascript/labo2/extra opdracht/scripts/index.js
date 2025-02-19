2d80de899a37307266d8626d763d277ece0367fa
aimaneajnifette <aimane.ajnifette@student.vives.be>
2025-02-19 01:23:04 +0100

    labo2

--- Javascript/labo2/extra opdracht/scripts/index.js
+++ Javascript/labo2/extra opdracht/scripts/index.js
@@ -1,3 +1,18 @@
 const setup = () => {
+    let aanKnop = document.getElementById('aan');
+    let uitKnop = document.getElementById('uit');
+    aanKnop.addEventListener('click', aandoen);
+    uitKnop.addEventListener('click', uitdoen);
+}
+
+const aandoen = () => {
+    let style = document.getElementById('style');
+    style.className = 'rood';
+
+}
+
+const uitdoen = () => {
+    let style = document.getElementById('style');
+    style.className = 'niets';
 }
 window.addEventListener("load", setup); 
